f94d0d33b88bed1b565a824babf84987dd99851c
Salhin Langhi <salhinlanghi@Salhins-MacBook-Pro.local>
2024-08-13 17:24:35 +0100

    project finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+import com.google.gson.Gson;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.htt
--- src/main/java/org/example/Name.java
+++ src/main/java/org/example/Name.java
@@ -1,2 +1,33 @@
-package org.example;public class Name {
+package org.example;
+
+public class Name {
+
+    private String name;
+    private int count;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
